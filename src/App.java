b193847e50f15b0715780d5c93674428c92d06a6
Hiago nunes pimentel <derundaclash665@gmail.com>
2023-12-30 20:48:58 -0300

    Expressão lambda inline

--- src/App.java
+++ src/App.java
@@ -1,29 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
-
 import entities.Product;
 
+public class App {
 
-public class App{
+    public static void main(String[] args) {
 
-		public static void main(String[] args){
-			
-			List<Product> list = new ArrayList<>();
+        List<Product> list = new ArrayList<>();
 
-			list.add(new Product("TV", 900.0));
-			list.add(new Product("Mouse", 50.00));
-			list.add(new Product("Tablet" , 350.50));
-			list.add(new Product("HD Case", 80.90));
+        list.add(new Product("TV", 900.0));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
 
-            Consumer<Product> cons = p -> {
-                p.setPrice(p.getPrice()* 1.1);
-            };
+        double factor = 1.1;
 
+        list.forEach(p -> p.setPrice(p.getPrice() * factor));
 
-            list.forEach(cons);
+        list.forEach(System.out::println);
 
-            list.forEach(System.out::println);
-
-        }
     }
+}
